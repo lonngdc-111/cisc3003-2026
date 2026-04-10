1111a9a1cf61b4622881b2de2ad29a3aeca331b4
lonngdc-111 <1280765521@qq.com>
2026-04-10 20:45:55 +0800

    Update left-nav.inc.php

--- cisc3003-sugex10-folder-DC229823YANGYUJIE/includes/left-nav.inc.php
+++ cisc3003-sugex10-folder-DC229823YANGYUJIE/includes/left-nav.inc.php
@@ -1,7 +1,7 @@
   <div class="mdl-layout__drawer mdl-color--blue-grey-800 mdl-color-text--blue-grey-50">
        <div class="profile">
            <img src="images/profile.jpg" class="avatar">
-           <h4>John Locke</h4>           
+           <h2>John Locke</h2>           
            <span>johnlocke@example.com</span>
        </div>
 
